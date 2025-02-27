06210c349fa684ba158963e12ef53382cc86a90a
jeannaegelen <jean.naegelen@gmail.com>
2025-02-27 13:56:46 +0100

    list controller added

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,15 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+  root to:"lists#index"
 end
+
+# Voici les actions utilisateur que nous voulons implémenter dans notre application :
+
+#     En tant qu’utilisateur, je peux voir toutes mes listes de films.
+#     En tant qu’utilisateur, je peux créer une liste de films.
+#     En tant qu’utilisateur, je peux voir les détails d’une liste de films.
+#     En tant qu’utilisateur, je peux marquer (bookmark) un film dans une liste de films.
+#     En tant qu’utilisateur, je peux détruire un bookmark.
+
+#     Attention les films seront créés dans la base de données avec une seed, il n’est donc pas nécessaire d’implémenter aucune action utilisateur autour du modèle de movie.
